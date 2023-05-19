ad7997294f39a611f0c0cadbab856ac1fb27814b
anhtuan2010 <tuan.hoanganh20102000@gmail.com>
2023-05-20 00:24:44 +0700

    update db.js

--- server/server/util/db.js
+++ server/server/util/db.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../config.env') });
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   host: 'localhost',
-  dialect: 'mysql', // Replace with your database dialect (e.g., mysql, postgres, sqlite, etc.)
+  dialect: 'mysql',
 });
 
 module.exports = sequelize;
