77b71680b5f5ec54f1b57a159428e1ff1fe484c5
anhtuan2010 <tuan.hoanganh20102000@gmail.com>
2023-05-31 23:23:44 +0700

    update server.js

--- server/server/server.js
+++ server/server/server.js
@@ -4,5 +4,9 @@ const app = require("./app");
 const path = require('path');
 dotenv.config({ path: "./config.env" });
 
+app.get("/", (req, res) => {
+    res.send("server");
+});
+
 const http = require("http").createServer(app);
 http.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
