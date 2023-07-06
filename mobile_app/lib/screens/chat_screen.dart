bf260efe353c563c04cc21192c72f339954004af
hiep2211 <hieplv2211@gmail.com>
2023-07-06 17:01:48 +0700

    create chat UI

--- mobile_app/lib/screens/chat_screen.dart
+++ mobile_app/lib/screens/chat_screen.dart
@@ -1,12 +1,16 @@
 import 'package:bluetooth_ecg/components/custom_text_form_field.dart';
 import 'package:bluetooth_ecg/constants/color_constant.dart';
 import 'package:bluetooth_ecg/models/doctor_info.dart';
+import 'package:bluetooth_ecg/routes/route.dart';
+import 'package:bluetooth_ecg/screens/chatdetail_screen.dart';
 import 'package:bluetooth_ecg/theme/app_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -19,7 +23,7 @@ class ChatScreen extends StatelessWidget {
         id: 1,
         userId: 2,
         departmentId: 3,
-        name: 'John Doe',
+        name: 'Nguyễn A',
         email: 'johndoe@example.com',
         password: 'password',
         phoneNo: '1234567890',
@@ -41,7 +45,7 @@ class ChatScreen extends StatelessWidget {
         id: 2,
         userId: 3,
         departmentId: 4,
-        name: 'Jane Doe',
+        name: 'Vũ B',
         email: 'janedoe@example.com',
         password: 'password',
         phoneNo: '1234567890',
@@ -62,7 +66,7 @@ class ChatScreen extends StatelessWidget {
       id: 3,
       userId: 4,
       departmentId: 5,
-      name: 'Bob Smith',
+      name: 'Trần C',
       email: 'bobsmith@example.com',
       password: 'password',
       phoneNo: '1234567890',
@@ -83,7 +87,7 @@ class ChatScreen extends StatelessWidget {
       id :4,
       userId :5,
       departmentId :6,
-      name :'Alice Smith' ,
+      name :'Mai D' ,
       email :'alicesmith@example.com' ,
       password :'password' ,
       phoneNo :'1234567890' ,
@@ -119,40 +123,55 @@ class ChatScreen extends StatelessWidget {
         child: 
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            
             children: [
               Container(
-
                 color: ColorConstant.blueA200,
                 width: double.infinity,
-                
                 padding: EdgeInsets.all(10),
                 margin: EdgeInsets.only(top:5),
                 child: SizedBox(
                   width: double.infinity ,
-                  height:70,
-                
-                 child: ListView.builder(
+                  height:70,              
+                  child: ListView.builder(
                    itemCount: doctors.length,
                    shrinkWrap: true,
                    addSemanticIndexes: true ,
                    scrollDirection: Axis.horizontal,
-                   physics: NeverScrollableScrollPhysics(),
-                  
+                   physics: ClampingScrollPhysics(),                 
                    itemBuilder: (context, index) {
                      return Container(
                       width: 80,
                       height: 80,            
                       padding: EdgeInsets.only(left: 10),
-                       child: Container(
-                         decoration: BoxDecoration(
-                          color: ColorConstant.whiteA700,
-                           borderRadius: BorderRadius.circular(30),
-                         ),
-                          padding: EdgeInsets.all(1),
-                           child: Image.asset(doctors[index].image,
-                              alignment: Alignment.center,     
-                           )
+                       child: Stack(
+                         children: [
+                          Container(
+                           decoration: BoxDecoration(
+                            color: ColorConstant.whiteA700,
+                             borderRadius: BorderRadius.circular(30),
+                           ),
+                            padding: EdgeInsets.all(1),
+                              child: GestureDetector(
+                                onTap: () => Navigator.push(context,
+                                MaterialPageRoute(builder:(context) =>  ChatDetail(),)
+                                ),
+                                child: Image.asset(doctors[index].image,
+                                  alignment: Alignment.center,     
+                               ),
+                             )
+                                                 ),
+                         Positioned(
+                          bottom:BorderSide.strokeAlignOutside,
+                          right: BorderSide.strokeAlignOutside,
+                          child: Container(
+                            width: 10,
+                            height: 10,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: ColorConstant.tertiary,
+                            ),
+                          ) )
+                         ]
                        ),
                      );
                   },),
@@ -171,11 +190,9 @@ class ChatScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                
+              children: [               
                 Padding(
                   padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
-                      
                   child: CustomTextFormField(
                   hintText: 'Search',
                   alignment: Alignment.topCenter,
@@ -191,7 +208,8 @@ class ChatScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       return Container(
                         width: double.infinity,
-                        height: 70,
+                        height: 100,
+                        padding: EdgeInsets.fromLTRB(15, 15, 5, 10),
                         decoration: BoxDecoration(
                           color: ColorConstant.whiteA700
                         ),
@@ -200,18 +218,56 @@ class ChatScreen extends StatelessWidget {
                           child: Row(
                             children: [
                               Container(
+                                margin: EdgeInsets.all(5),
                                 child: Image.asset(doctors[index].image),
                                 color: ColorConstant.whiteA700,
                               ),
-                              Column(
-                                children: [
-                                  Text(doctors[index].name),
-                                  Text('Recent message')
-                                ],
+                              Container(
+                                width: size.width / 1.4,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(doctors[index].name,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15
+                                          ),                                
+                                        ),
+                                        Text('20.00',
+                                          style: TextStyle(
+                                            fontSize: 12
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Tin nhắn mới'),
+                                        Container(
+                                          padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(20),
+                                            color: ColorConstant.primary,
+                                          ),
+                                          child: Text('1',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w900,
+                                          ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               )
                             ],
                           ),
-                  )
+                                        )
                 );                
               },
             )
