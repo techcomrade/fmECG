deaf8f29f6e93e5bce30bf08230dd75f9150fa04
Huyz109 <phamqhuy2001@gmail.com>
2023-12-07 10:27:35 +0700

    feature(server): add line chart component

--- server/Views/pages/Dashboard.jsx
+++ server/Views/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import SingleCard from '../components/SingleCard';
 // import MileChart from '../components/MileChart';
 
 import { RiUser2Line, RiUserHeartLine, RiNewspaperLine, RiRecordCircleLine } from 'react-icons/ri';
+import { LineChartComponent } from "../components/LineChart";
 
 const dashboardStyles = {
   marginTop: "20px",
@@ -114,6 +115,51 @@ const Dashboard = () => {
         });
     }, []);
 
+    // Example data
+    const dataChart = [
+      {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+    ];
     
   return (
     <div style={dashboardStyles} className="dashboard">
@@ -124,7 +170,7 @@ const Dashboard = () => {
           <SingleCard item={newsObj} />
           <SingleCard item={ecgObj} />
         </div>
-
+        <LineChartComponent data={dataChart} />
       {/* <div style={staticsStyles} className="statics">
         <div style={statsStyles} className="stats">
           <h3 style={statsTitleStyles} className="stats__title">User Statistics</h3>
