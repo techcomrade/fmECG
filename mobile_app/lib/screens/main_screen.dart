9768a843e85ddbc2dfca4df2b8ab4f76a671204e
dath7 <hoangdat24112002@gmail.com>
2023-12-13 16:25:13 +0700

    call python code from native java

--- mobile_app/lib/screens/main_screen.dart
+++ mobile_app/lib/screens/main_screen.dart
@@ -1,34 +1,48 @@
 import 'dart:async';
 
-import 'package:bluetooth_ecg/constants/color_constant.dart';
 import 'package:bluetooth_ecg/controllers/firebase_messages_controller.dart';
-import 'package:bluetooth_ecg/generated/l10n.dart';
-import 'package:bluetooth_ecg/main.dart';
-import 'package:bluetooth_ecg/providers/auth_provider.dart';
 import 'package:bluetooth_ecg/screens/chat_screens/chat_screen.dart';
 import 'package:bluetooth_ecg/screens/history_screens/history_screen.dart';
 import 'package:bluetooth_ecg/screens/home_screen.dart';
 import 'package:bluetooth_ecg/screens/user_screens/user_profile_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
 
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
-Color LIME = Color(0xFF094D55);
+Color LIME = const Color(0xFF094D55);
 
 class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
+  static const platform = MethodChannel("com.example.method_channel/java");
+
+  final int _currentIndex = 0;
 
   final List<Widget> _screens = [
     HomeScreen(),
     HistoryScreen(),
-    ChatScreen(),
+    const ChatScreen(),
     UserProfileScreen()
   ];
+
+  String _text = '';
+  Future<void> _getText() async {
+    String text;
+    try {
+      final result = await platform.invokeMethod<String>('helloWorldPython');
+      text = result!;
+    } on PlatformException catch (e) {
+      text = "Failed to get text: '${e.message}'.";
+    }
+
+    setState(() {
+      _text = text;
+    });
+  }
 
   @override
   void initState() {
@@ -65,34 +79,46 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: PhosphorIcon(PhosphorIcons.regular.house),
-            label: S.current.home,
+        resizeToAvoidBottomInset: true,
+        body: Center(
+          child: Column(
+            children: [
+              Text(_text),
+              ElevatedButton(
+                onPressed: _getText,
+                child: const Text("Call python "),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
-          BottomNavigationBarItem(
-            icon: PhosphorIcon(PhosphorIcons.regular.chartLine),
-            label: S.current.history,
-          ),
-          BottomNavigationBarItem(
-            icon: PhosphorIcon(PhosphorIcons.regular.chatCircle),
-            label: S.current.chat,
-          ),
-          BottomNavigationBarItem(
-            icon: PhosphorIcon(PhosphorIcons.regular.userCircle),
-            label: S.current.profile,
-          ),
-        ],
-      ),
-    );
+        )
+        // _screens[_currentIndex],
+        // bottomNavigationBar: BottomNavigationBar(
+        //   currentIndex: _currentIndex,
+        //   onTap: (int index) {
+        //     setState(() {
+        //       _currentIndex = index;
+        //     });
+        //   },
+        //   items: [
+        //     BottomNavigationBarItem(
+        //       icon: PhosphorIcon(PhosphorIcons.regular.house),
+        //       label: S.current.home,
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: PhosphorIcon(PhosphorIcons.regular.chartLine),
+        //       label: S.current.history,
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: PhosphorIcon(PhosphorIcons.regular.chatCircle),
+        //       label: S.current.chat,
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: PhosphorIcon(PhosphorIcons.regular.userCircle),
+        //       label: S.current.profile,
+        //     ),
+        //   ],
+        // ),
+        );
   }
 }
