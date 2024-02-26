5ec7ba7eca323ad4324009bc9ccfad15dfcff991
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-02-26 21:20:10 +0700

    update server.js

--- server-web/server.js
+++ server-web/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
 const routes = require('./routes/index');
+const port = process.env.APP_PORT || 3000;
+const host = process.env.APP_HOST || 'localhost';
+
 require('dotenv').config({ path: '.env' })
 
 app.set('view engine', 'ejs');
@@ -10,11 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.raw());
 app.use(express.static('public'));
-
+app.use(cors());
+app.use(compression());
 // Router
 app.use('/api', routes);
 
-app.listen(process.env.APP_PORT, () => {
-    console.log(`Server is running at http://${process.env.APP_HOST}:${process.env.APP_PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running at http://${host}:${port}`);
 })
 
