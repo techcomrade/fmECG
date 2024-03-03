779e23247f3880890995a757c419f6c4837336b8
thaidmfinnick <thai.dm279@gmail.com>
2024-03-03 15:41:46 +0700

    improve(mobile): update user profile screen by Hoang

--- mobile_app/lib/screens/user_screens/user_profile_screen.dart
+++ mobile_app/lib/screens/user_screens/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+import '../../constants/color_constant.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
@@ -10,28 +11,11 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
   TextEditingController _nameController = TextEditingController();
-  TextEditingController _emailController  = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
   TextEditingController _bioController = TextEditingController();
   TextEditingController _locationController = TextEditingController();
 
-  static const platform = MethodChannel("com.example.method_channel/java");
-
   // Get battery level.
-  String _batteryLevel = 'Unknown battery level.';
-
-  Future<void> _getBatteryLevel() async {
-    String batteryLevel;
-    try {
-      final result = await platform.invokeMethod<int>('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
-    } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
-    }
-
-    setState(() {
-      _batteryLevel = batteryLevel;
-    });
-  }
 
   @override
   void initState() {
@@ -51,89 +35,139 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     super.dispose();
   }
 
+  Widget _buildListTile({required String title, required IconData icon, required Color iconColor, VoidCallback? onTap}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4), // Khoảng cách giữa các ListTile
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white10), // Viền xám cho mỗi ListTile
+        borderRadius: BorderRadius.circular(10), // Góc bo tròn
+      ),
+      child: ListTile(
+        title: Text(title, style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize:13,
+        ),
+        )
+        ,
+        leading: Icon(icon, color: iconColor),
+        onTap: onTap,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Test native"),
+          title: const Text("User Profile"),
+          backgroundColor: ColorConstant.primary, // Màu sắc của AppBar
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(_batteryLevel),
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(13.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/doctor.png'),
+                  backgroundColor: Colors.transparent, // Set the background color to transparent
+
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Viet Hoang',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'Hà Nội, Việt Nam',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      InfoCard(title: 'Age', value: '30', unit: 'years'),
+                      InfoCard(title: 'Weight', value: '90', unit: 'kg'),
+                      InfoCard(title: 'Height', value: '190', unit: 'cm'),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                _buildListTile(title: 'Personal Info', icon: Icons.person, iconColor: Colors.blue, onTap: () {}),
+                _buildListTile(title: 'My Appointments', icon: Icons.calendar_today, iconColor: Colors.orange, onTap: () {}),
+                _buildListTile(title: 'My Doctors', icon: Icons.local_hospital, iconColor: Colors.red, onTap: () {}),
+                _buildListTile(title: 'EHR Files', icon: Icons.folder, iconColor: Colors.green, onTap: () {}),
+                _buildListTile(title: 'Wallet', icon: Icons.account_balance_wallet, iconColor: Colors.purple, onTap: () {}),
+              ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _getBatteryLevel,
-          tooltip: 'Increment',
-          child: const Icon(Icons.battery_0_bar),
         ),
       ),
     );
-    // return SingleChildScrollView(
-    //   child: Padding(
-    //     padding: EdgeInsets.all(16.0),
-    //     child: Column(
-    //       crossAxisAlignment: CrossAxisAlignment.center,
-    //       children: [
-    //         SizedBox(height: 20),
-    //         CircleAvatar(
-    //           radius: 60,
-    //           backgroundImage: AssetImage('assets/images/fm_ecg.png'),
-    //         ),
-    //         SizedBox(height: 20),
-    //         TextField(
-    //           controller: _nameController,
-    //           decoration: InputDecoration(
-    //             labelText: 'Name',
-    //             prefixIcon: Icon(Icons.person),
-    //           ),
-    //         ),
-    //         SizedBox(height: 10),
-    //         TextField(
-    //           controller: _emailController,
-    //           decoration: InputDecoration(
-    //             labelText: 'Email',
-    //             prefixIcon: Icon(Icons.email),
-    //           ),
-    //         ),
-    //         SizedBox(height: 10),
-    //         TextField(
-    //           controller: _bioController,
-    //           decoration: InputDecoration(
-    //             labelText: 'Bio',
-    //             prefixIcon: Icon(Icons.article),
-    //           ),
-    //         ),
-    //         SizedBox(height: 10),
-    //         TextField(
-    //           controller: _locationController,
-    //           decoration: InputDecoration(
-    //             labelText: 'Location',
-    //             prefixIcon: Icon(Icons.location_on),
-    //           ),
-    //         ),
-    //         SizedBox(height: 20),
-    //         ElevatedButton(
-    //           onPressed: () async {
-    //             // Save the updated user profile information
-    //             // String name = _nameController.text;
-    //             // String email = _emailController.text;
-    //             // String bio = _bioController.text;
-    //             // String location = _locationController.text;
-
-    //             // await context.read()<AuthProvider>().logout();
-    //             await Provider.of<AuthProvider>(context, listen: false).logoutUser();
-    //             // Perform the necessary actions to save the updated information
-    //           },
-    //           child: Text('Logout'),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
+// Custom InfoCard widget with Title
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String value;
+  final String unit;
+
+  const InfoCard({
+    Key? key,
+    required this.title,
+    required this.value,
+    required this.unit,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(5),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Container(
+        width: 150,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.black45,
+                fontSize: 10,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              '$value $unit',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
