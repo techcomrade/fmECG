0479c4865f09e02fe49b396e4fc222cea9ddf99f
Duong <duongtoan1k99@gmail.com>
2024-03-07 18:34:34 +0700

    register api

--- server-web/models/CommonModel.js
+++ server-web/models/CommonModel.js
@@ -1,7 +1,7 @@
 const knex = require('../config/knex.js');
-const bcrypt = require("bcrypt");
+
 class CommonModel {
-    async queryDB(sql){
+    async executeQuery(sql){
         try {
             let data = await knex.raw(sql);
             if(data[0].length != 0) return data[0];
@@ -10,9 +10,7 @@ class CommonModel {
         catch(err) {
             console.error(err);
             return false;
-    }
-    
-}
-    
+        }
+    }  
 }
 module.exports = CommonModel;
--- server-web/routes/UserRouter.js
+++ server-web/routes/UserRouter.js
@@ -1,5 +1,8 @@
+const express = require('express')
+const UserController = require('../controllers/UserController')
+const router = express.Router()
 
+// router.get('/', UserController.getAllData)
+router.post('/register', UserController.register)
 
-class UserRouter {
-
-}
+module.exports = router;
