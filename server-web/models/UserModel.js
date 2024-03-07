72c0ca1bfa9de9e0d7b48f0ddf07c00999ee03c1
Duong <duongtoan1k99@gmail.com>
2024-03-07 19:11:38 +0700

    update UserModel.js

--- server-web/models/UserModel.js
+++ server-web/models/UserModel.js
@@ -8,10 +8,8 @@ class UserModel extends CommonModel{
         return await this.executeQuery(`INSERT INTO account (id, email, password, create_time) VALUES ('${dataInsert.id}', '${dataInsert.email}', '${dataInsert.password}', '${dataInsert.create_time}')`)
     }
     async checkDuplicate(email) {
-        // const check = await this.executeQuery(`SELECT * FROM account WHERE email = '${email}'`)
-        // return check.length > 0
-        const emails = await this.executeQuery(`SELECT COUNT(*) FROM account WHERE email = '${email}'`)
-        return emails[0]['COUNT(*)'] > 0; 
+        const check = await this.executeQuery(`SELECT * FROM account WHERE email = '${email}'`)
+        return check.length > 0;
     }
 }
 
