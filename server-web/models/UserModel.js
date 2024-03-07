c4a9bc3000e9db093f1022a73a8a68f2057f4265
Tmtuan <92735162+hwt75@users.noreply.github.com>
2024-03-07 16:23:33 +0700

    Revert "Server web"

--- server-web/models/UserModel.js
+++ server-web/models/UserModel.js
@@ -1,19 +1,7 @@
-const database = require('../config/database');
-const CommonModel = require('./CommonModel');
+const CommonModel = require("./CommonModel");
 
-class User extends CommonModel {
-
-    getTableName() { return 'users'; }
-    getAllData() {
-        return `SELECT * FROM account WHERE delete_flag = 0`;
-    }
-    displayById(id) {
-        return `SELECT * FROM account WHERE id = ${id} and delete_flag = 0`;
-    }
-    findOneById(id) {
-        return `SELECT * FROM account WHERE id = ${id}`;
-        
+class UserModel extends CommonModel{
+    async getAllData(){
+        return await this.executeQuery("SELECT * FROM nguoi_dung");
     }
 }
-
-module.exports = new User;
