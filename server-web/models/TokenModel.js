4e7186dac5e921c53b25c81776fc097460edc5eb
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-03-14 22:55:44 +0700

    update Model

--- server-web/models/TokenModel.js
+++ server-web/models/TokenModel.js
@@ -2,10 +2,18 @@ const CommonModel = require("./CommonModel");
 
 class TokenModel extends CommonModel{
     async getAllData(){
-        return await this.executeQuery(`SELECT * FROM account`);
+        return `SELECT * FROM tokens;`;
     }
-
-   
+    async add(token) {
+        return `INSERT INTO tokens (id, account_id, access_token, refresh_token, created_at, updated_at)
+        VALUES ('${token.id}', '${token.account_id}', '${token.access_token}','${token.refresh_token}','${token.created_at}', '${token.updated_at}');`
+    }
+    async deleteById(id) {
+        return `DELETE FROM tokens WHERE id = '${id};`
+    }
+    async updateById(token){
+        return `UPDATE tokens SET account_id = '${token.account_id}',access_token = '${token.access_token}', refresh_token = '${token.refresh_token}', created_at = '${token.created_at}',updated_at = '${token.updated_at}', account_id = '${token.account_id}', account_id = '${token.account_id}'  WHERE id = '${token.id};`;
+    }
 
 }
 
