96d4e017c8e131317f784d454f7e5f473c7e8138
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-03-14 14:05:38 +0700

    test migration with knex

--- server-web/server.js
+++ server-web/server.js
@@ -1,28 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const compression = require('compression');
-require('dotenv').config({ path: '.env' })
-const routes = require('./routes/index');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const compression = require("compression");
+require("dotenv").config({ path: ".env" });
+const routes = require("./routes/index");
 const port = process.env.APP_PORT || 3000;
-const host = process.env.APP_HOST || 'localhost';
+const host = process.env.APP_HOST || "localhost";
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 
+const db = knex(knexConfig);
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json({ type: 'application/json' }))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.raw());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cors());
 app.use(compression());
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb'}));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb" }));
 
 // Router
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://${host}:${port}`);
-})
+  console.log(`Server is running at http://${host}:${port}`);
+});
 
+db.migrate
+  .latest()
+  .then(() => {
+    console.log("Migrations ran successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+    return;
+  });
