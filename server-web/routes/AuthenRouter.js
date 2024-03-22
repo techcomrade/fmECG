baef4d25209d4443b514d0971304ad79e80114ab
hwt75 <dnthang300@gmail.com>
2024-03-22 22:52:05 +0700

    #70 add base code middleware

--- server-web/routes/AuthenRouter.js
+++ server-web/routes/AuthenRouter.js
@@ -4,6 +4,6 @@ const AuthenController = require("../controllers/AuthenController");
 const router = express.Router();
 
 router.get("/", AuthenController.getAllData);
-router.post("/register", AuthenController.register);
+router.post("/register", AuthenController.token, AuthenController.validationUser, AuthenController.register);
 
 module.exports = router;
