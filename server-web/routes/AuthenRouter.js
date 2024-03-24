357f78385fd3ad6852568701e5b479bc85b49623
Duong <duongtoan1k99@gmail.com>
2024-03-24 10:26:03 +0700

    validate user info when register in AuthenMiddleware

--- server-web/routes/AuthenRouter.js
+++ server-web/routes/AuthenRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const AuthenController = require("../controllers/AuthenController");
+const AuthenMiddleware = require("../middlewares/AuthenMiddleware");
 
 const router = express.Router();
 
 router.get("/", AuthenController.getAllData);
-router.post("/register", AuthenController.register);
+router.post("/register", AuthenMiddleware.validateUser, AuthenController.register);
 
 module.exports = router;
