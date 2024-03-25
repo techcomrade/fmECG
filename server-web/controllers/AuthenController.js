484abeaeb97f798c4abd78ebbf04d743ed05ce38
hwt75 <dnthang300@gmail.com>
2024-03-25 16:42:58 +0700

    #57 removed code that was unused  in model

--- server-web/controllers/AuthenController.js
+++ server-web/controllers/AuthenController.js
@@ -20,48 +20,6 @@ class AuthenController {
       return res.status(400).json("Register error");
     }
   }
-  // async login(req, res, next) {
-  //   await UserModel.findByEmail(req.body.email)
-  //     .then((user) => {
-  //       if (user[0]) {
-  //         bcrypt.compare(req.body.pass, user[0].pass, (err, same) => {
-  //           if (err) throw err;
-  //           if (same) {
-  //             const token = jwt.sign(
-  //               {
-  //                 id: user[0].id,
-  //                 email: user[0].email,
-  //                 pass: user[0].pass,
-  //                 create_time: user[0].create_time,
-  //               },
-  //               process.env.JWT_KEY,
-  //               {
-  //                 expiresIn: "30s",
-  //               }
-  //             );
-  //             AuthenModel.insertToken({
-  //               id: uuidv4(),
-  //               token: token,
-  //               create_time: new Date().toDateString(),
-  //               delete_flag: 0,
-  //             });
-  //             res.status(200).json({
-  //               id: uuidv4(),
-  //               token: token,
-  //               create_time: new Date().toDateString(),
-  //               delete_flag: 0,
-  //               message: "Token added successfully",
-  //             });
-  //           }
-  //         });
-  //       }
-  //     })
-
-  //     .catch((err) => {
-  //       console.log(error);
-  //       res.status(403).json({ message: err });
-  //     });
-  // }
 
   async getAllData(req, res) {
     await AuthenService.getAll()
