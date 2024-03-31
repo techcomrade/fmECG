19f4a2780b1fed8aeef12db3c2d55ebb1d951650
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-03-31 13:47:07 +0700

    #85 fix(back-end): fix message fe

--- server-web/controllers/BloodPressureController.js
+++ server-web/controllers/BloodPressureController.js
@@ -11,21 +11,32 @@ class BloodPressureController {
             message: "Get all bloodPressure records",
             metadata: rec,
           });
-        return res.status(400).json("No record found");
+        return res.status(400).json({
+          message: "No record found",
+        });
       })
       .catch((err) => {
-        return res.status(400).json("get records failed");
+        return res.status(400).json({
+          message: "get records failed",
+        });
       });
   }
   async add(req, res) {
     const record = req.body;
     const checkExistRecord = await RecordService.getRecordById(record.rec_id);
     if (!checkExistRecord.dataValues)
-      return res.status(400).json("no record_id found");
+      return res.status(400).json({
+        message: "no record_id found",
+      });
     await BloodPressureService.add(record)
       .then((checked) => {
-        if (checked) return res.status(200).json("add record successfully");
-        return res.status(500).json("err server add failed");
+        if (checked)
+          return res.status(200).json({
+            message: "add record successfully",
+          });
+        return res.status(500).json({
+          message: "err server add failed",
+        });
       })
       .catch((err) => {
         return res.status(400).json("add record failed");
@@ -38,13 +49,19 @@ class BloodPressureController {
         record_id
       );
       if (!checkExistRecord.dataValues)
-        return res.status(400).json("no record_id found");
+        return res.status(400).json({
+          message: "no record_id found",
+        });
       await BloodPressureService.deleteById(record_id)
         .then(() => {
-          return res.status(200).json("delete record successfully");
+          return res.status(200).json({
+            message: "delete record successfully",
+          });
         })
         .catch((err) => {
-          return res.status(500).json("no record found");
+          return res.status(500).json({
+            message: "no record found",
+          });
         });
     }
   }
@@ -53,16 +70,22 @@ class BloodPressureController {
     const record = req.body;
     let checkExistBPRecord = await BloodPressureService.getRecordById(id);
     if (!checkExistBPRecord.dataValues)
-      return res.status(400).json("no recordBP_id found");
+      return res.status(400).json({
+        message: "no recordBP_id found",
+      });
     const checkExistRecord = await RecordService.getRecordById(record.rec_id);
     if (!checkExistRecord.dataValues)
       return res.status(400).json("no record_id found");
     await BloodPressureService.updateById(record, id)
       .then(() => {
-        return res.status(200).json("update record successfully");
+        return res.status(200).json({
+          message: "update record successfully",
+        });
       })
       .catch((err) => {
-        return res.status(500).json("error updating record");
+        return res.status(500).json({
+          message: "error updating record",
+        });
       });
   }
 }
