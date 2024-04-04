220210ef29fd81b2d810b8b744fee4445fb90ef0
CozDung29 <codung2909@gmail.com>
2024-04-04 14:39:12 +0700

    update heart_rec api #78

--- server-web/controllers/HeartRecController.js
+++ server-web/controllers/HeartRecController.js
@@ -2,16 +2,44 @@ const { Model } = require("sequelize");
 const HeartRecService = require("../services/HeartRecSevice");
 
 class HeartRecController {
-    async getAllData(req, res){
-        await HeartRecService.getAllData()
-        .then((heart_rec) => {
-            if (heart_rec.length) return res.status(200).json(heart_rec);
-            else return res.status(400).json("Invalid HeartRec");
-        })
-        .catch((error) => {
-            return res.status(400).json(error);
+  async getAllData(req, res) {
+    console.log(`[P]:::Get all heart_rec: `);
+    const heart_rec = await HeartRecService.getAllData();
+    return res.status(200).json({
+      message: "Get all heart_rec successful!",
+      metadata: heart_rec,
+    });
+  }
+
+  async getHeartRecByEmail(req, res) {
+    console.log(`[P]:::Get all heart_rec by email: `, req.body.email);
+    const foundHeartRec = await HeartRecService.getHeartRecByEmail(req.body.email);
+    if(foundHeartRec) {
+        return res.status(200).json({
+          message: 'Get heart_rec by email successful!',
+          metadata: foundHeartRec
         });
-    }
+      }
+      else {
+        return res.status(404).json({
+          message: 'Heart_rec not existed!'
+        });
+      }
+  }
+
+  async delebyId(req, res) {
+    console.log(` [P]:::Delete heart_rec by id: `, req.body.id);
+    const result = await HeartRecController.delebyId(req.body.id);
+    if (result) {
+        return res.status(200).json({
+          message: "Delete heart_rec successful!",
+        });
+      } else {
+        return res.status(404).json({
+          message: "Error when delete heart_rec!",
+        });
+      }
+  }
 }
 
 module.exports = new HeartRecController();
--- server-web/routes/HeartRecRouter.js
+++ server-web/routes/HeartRecRouter.js
@@ -5,5 +5,8 @@ const HeartRecMiddleware = require("../middlewares/HeartRecMiddleware");
 const router = express.Router();
 
 router.get("/", HeartRecController.getAllData);
+router.get(":email", HeartRecController.getHeartRecByEmail);
+router.delete("/:id", HeartRecController.delebyId)
+
 
 module.exports = router;
