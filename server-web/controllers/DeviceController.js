0561604b68477899fe7fdc1ed896f1ac8498fbc9
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-04-08 21:18:42 +0700

    #18 improve(back-end): add function getHeartByDeviceId

--- server-web/controllers/DeviceController.js
+++ server-web/controllers/DeviceController.js
@@ -2,7 +2,7 @@ const DeviceService = require("../services/DeviceService");
 const UserService = require("../services/UserService");
 const Record = require("../services/RecordService");
 const BloodPressureRecord = require("../services/BloodPressureService");
-//const HeartRecord = require("../services/HeartRecordService");
+const HeartRecord = require("../services/HeartRecService");
 
 class DeviceController {
   async getAllData(req, res) {
@@ -76,67 +76,122 @@ class DeviceController {
             message: "delete device failed",
           });
         });
-    }
+    } else
+      return res.status(400).json({
+        message: "no device id selected",
+      });
   }
   async update(req, res) {
     console.log(`[P]:Update device data`, req.body);
     const id = req.params.id;
     const device = req.body;
-    let checkExistDevice = await DeviceService.checkDevice(id);
-    if (checkExistDevice?.dataValues) {
-      const checkExistUser = await UserService.getUserById(device.user_id);
-      if (!checkExistUser[0]?.dataValues) {
+    if (id) {
+      let checkExistDevice = await DeviceService.checkDevice(id);
+      if (checkExistDevice?.dataValues) {
+        const checkExistUser = await UserService.getUserById(device.user_id);
+        if (!checkExistUser[0]?.dataValues) {
+          return res.status(400).json({
+            message: "no user found",
+          });
+        }
+        await DeviceService.updateById(device, id)
+          .then(() => {
+            return res.status(200).json({
+              message: "update device successfully",
+            });
+          })
+          .catch((err) => {
+            return res.status(500).json({
+              message: "update device failed",
+            });
+          });
+      } else
         return res.status(400).json({
-          message: "no user found",
-        });
-      }
-      await DeviceService.updateById(device, id)
-        .then(() => {
-          return res.status(200).json({
-            message: "update device successfully",
-          });
-        })
-        .catch((err) => {
-          return res.status(500).json({
-            message: "update device failed",
-          });
+          message: "no device found",
         });
     } else
       return res.status(400).json({
-        message: "no device found",
+        message: "no device id selected",
       });
   }
   async getBloodPressureRecordByDeviceId(req, res) {
     console.log(`[P]:::Get Blood Pressure record by Device Id`, req.params.id);
     const id = req.params.id;
-    let checkExistDevice = await DeviceService.checkDevice(id);
-    if (!checkExistDevice?.dataValues) {
-      return res.status(400).json({
-        message: "device not found",
+    if (id) {
+      let checkExistDevice = await DeviceService.checkDevice(id);
+      if (!checkExistDevice?.dataValues) {
+        return res.status(400).json({
+          message: "device not found",
+        });
+      }
+      let checkExistRecord = await Record.getRecordByDeviceId(id);
+      if (!checkExistRecord[0]?.dataValues) {
+        return res.status(400).json({
+          message: "record not found",
+        });
+      }
+      let record_id = checkExistRecord[0].dataValues.id;
+      let checkExistBPRecord = await BloodPressureRecord.getRecordByRecordId(
+        record_id
+      );
+      if (!checkExistBPRecord[0]?.dataValues) {
+        return res.status(400).json({
+          message: "No BloodPressure record by this device",
+        });
+      }
+      let records = [];
+      checkExistBPRecord.forEach((record) => {
+        records.push(record.dataValues);
       });
-    }
-    let checkExistRecord = await Record.getRecordByDeviceId(id);
-    if (!checkExistRecord[0]?.dataValues) {
-      return res.status(400).json({
-        message: "record not found",
+      //console.log(records);
+      return res.status(200).json({
+        message: "get BP record by device successfully",
+        metadata: records,
       });
-    }
-    let record_id = checkExistRecord[0].dataValues.id;
-    let checkExistBPRecord = await BloodPressureRecord.getRecordByRecordId(record_id);
-    if (!checkExistBPRecord[0]?.dataValues) {
+    } else
       return res.status(400).json({
-        message: "No BloodPressure record by this device",
+        message: "No device id selected",
       });
-    }
-    let records = [];
-    checkExistBPRecord.forEach(record => {
-      records.push(record.dataValues);
-    })
-    console.log(records);
-    return res.status(200).json({
-      message: "get BP record by device successfully",
-      metadata: records,
-    });
+  }
+  async getHeartRecordByDeviceId(req, res) {
+    console.log(`[P]:::Get Heart record by Device Id`, req.params.id);
+    const id = req.params.id;
+    if (id) {
+      let checkExistDevice = await DeviceService.checkDevice(id);
+      if (!checkExistDevice?.dataValues) {
+        return res.status(400).json({
+          message: "device not found",
+        });
+      }
+      let checkExistRecord = await Record.getRecordByDeviceId(id);
+      if (!checkExistRecord[0]?.dataValues) {
+        return res.status(400).json({
+          message: "record not found",
+        });
+      }
+      let record_id = checkExistRecord[0].dataValues.id;
+      let checkExistHeartRecord = await HeartRecord.getHeartRecByRecordId(
+        record_id
+      );
+      console.log(checkExistHeartRecord, record_id);
+      if (!checkExistHeartRecord[0]?.dataValues) {
+        return res.status(400).json({
+          message: "No Heart record by this device",
+        });
+      }
+      let records = [];
+      checkExistHeartRecord.forEach((record) => {
+        records.push(record.dataValues);
+      });
+      //console.log(records);
+      return res.status(200).json({
+        message: "get BP record by device successfully",
+        metadata: records,
+      });
+    } else
+      return res.status(400).json({
+        message: "no device id selected",
+      });
   }
 }
 
--- server-web/routes/DeviceRouter.js
+++ server-web/routes/DeviceRouter.js
@@ -10,5 +10,6 @@ router.post("/create", DeviceMiddleware.validateData, DeviceController.add);
 router.post("/update/:id", DeviceMiddleware.validateData, DeviceController.update);
 
 router.get("/BPrecord/:id", DeviceController.getBloodPressureRecordByDeviceId);
+router.get("/HeartRec/:id", DeviceController.getHeartRecordByDeviceId);
 
 module.exports = router;
