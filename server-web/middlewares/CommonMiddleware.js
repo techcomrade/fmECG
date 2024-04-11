b286eb42b92cef0c514de73a343e86f4aa502232
Duong <duongtoan1k99@gmail.com>
2024-04-11 10:40:52 +0700

    improve(back-end): apiLimiter using redis

--- server-web/middlewares/CommonMiddleware.js
+++ server-web/middlewares/CommonMiddleware.js
@@ -1,27 +1,32 @@
-const rateLimit = [];
+let requests;
+const client = require("../config/redis");
 class CommonMiddleware {
   validationToken(req, res, next) {
     next();
   }
-  apiLimiter(req, res, next) {
-    const ip = req.ip;
-    const date = Date.now();
-    if (!rateLimit[ip]) {
-      rateLimit[ip] = { time: date, requests: 1 };
+  async apiLimiter(req, res, next) {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    if ((await client.get(ip)) === null) {
+      requests = 1;
+      await client.set(ip, requests);
+      await client.expire(ip, 1);
+      console.log("Number of reqs:", await client.get(ip));
+      console.log("Time left:", 1);
       return next();
-    }
-    const delta = date - rateLimit[ip].time;
-    if (delta < 1000) {
-      rateLimit[ip].requests++;
-      if (rateLimit[ip].requests > 5) {
+    } else {
+      const requests = await client.incr(ip, (err, result) => {
+        if (err) console.log(err);
+        return result;
+      });
+      console.log("Number of reqs:", requests);
+      console.log("Time left:", await client.ttl(ip));
+      if (requests > 5) {
         return res.status(429).json({
           message: "Too many requests, please try again later",
         });
       }
       return next();
     }
-    rateLimit[ip] = { time: date, requests: 1 };
-    return next();
   }
 }
 module.exports = new CommonMiddleware();
