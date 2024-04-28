f04bad7d5ed58a623d3e55996f99fddb4768cf7c
hwt75 <dnthang300@gmail.com>
2024-04-28 16:06:19 +0700

    #35 feature(back-end): add spo2 sound table into sql

---
+++ server-web/models/Spo2RecModel/Spo2RecDTO.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize");
+const Record = require("../RecordModel/RecordDTO");
+
+const Spo2_rec = sequelize.define(
+  "spo2_rec",
+  {
+    id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    rec_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      references: {
+        model: Record,
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true
+  }
+);
+
+module.exports = Spo2_rec;
