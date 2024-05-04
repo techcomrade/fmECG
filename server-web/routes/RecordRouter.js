0632ae911d3887aefc7eb2460b0dca2bcfaedb9d
Duong <duongtoan1k99@gmail.com>
2024-05-04 16:52:11 +0700

    improve(back-end): add RecordMiddleware

--- server-web/routes/RecordRouter.js
+++ server-web/routes/RecordRouter.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const RecordController = require("../controllers/RecordController");
+const RecordMiddleware = require("../middlewares/RecordMiddleware");
 const router = express.Router();
 
 router.get("", RecordController.getAll);
-router.post("", RecordController.createRecord);
-router.get("/:recordId", RecordController.getRecordById);
-router.get("/device/:deviceId", RecordController.getRecordByDeviceId);
-router.get("/user/:userId", RecordController.getRecordByUserId);
-router.get("/start/:time", RecordController.getRecordByStartTime);
-router.get("/end/:time", RecordController.getRecordByEndTime);
+router.post("", RecordMiddleware.validateRecord, RecordController.createRecord);
+router.get("/:recordId", RecordMiddleware.checkId, RecordController.getRecordById);
+router.get("/device/:deviceId", RecordMiddleware.checkDeviceId, RecordController.getRecordByDeviceId);
+router.get("/user/:userId", RecordMiddleware.checkUserId, RecordController.getRecordByUserId);
+router.get("/start/:time", RecordMiddleware.checkStartTime, RecordController.getRecordByStartTime);
+router.get("/end/:time", RecordMiddleware.checkEndTime, RecordController.getRecordByEndTime);
 router.post("/update/:recordId", RecordController.updateRecordById);
 router.delete("", RecordController.deleteRecordById);
 
--- server-web/services/RecordService.js
+++ server-web/services/RecordService.js
@@ -3,6 +3,7 @@ const RecordRepository = require("../models/RecordModel/RecordRepository");
 const HeartRecRepository = require("../models/HeartRecModel/HeartRecRepository");
 const BloodPressureRepository = require("../models/BloodPressureModel/BloodPressureRepository");
 const { v4: uuidv4 } = require("uuid");
+const Joi = require("joi")
 class RecordService extends CommonService {
   async getAll() {
     return await RecordRepository.getAllData();
@@ -13,33 +14,43 @@ class RecordService extends CommonService {
     return await RecordRepository.add(record);
   }
 
+  validateRecord(record) {
+    const schema = Joi.object({
+      user_id: Joi.string().required(),
+      device_id: Joi.string().required(),
+      device_type: Joi.number().integer().min(0).max(100).required(),
+      start_time: Joi.number().integer().required(),
+      end_time: Joi.number()
+        .integer()
+        .greater(Joi.ref("start_time"))
+        .required(),
+      data_rec_url: Joi.string().required(),
+    });
+    return schema.validate(record);
+  }
+
   async getRecordById(id) {
-    const record = await RecordRepository.getRecordById(id);
-    if (record.length === 0) return false;
-    return record;
+    const recordById = await RecordRepository.getRecordById(id);
+    return recordById;
   }
 
   async getRecordByUserId(id) {
     const recordByUser = await RecordRepository.getRecordByUserId(id);
-    if (recordByUser.length === 0) return false;
     return recordByUser;
   }
 
   async getRecordByDeviceId(id) {
     const recordByDevice = await RecordRepository.getRecordByDeviceId(id);
-    if (recordByDevice.length === 0) return false;
     return recordByDevice;
   }
 
-  async getRecordByStartTime(id) {
-    const recordByStartTime = await RecordRepository.getRecordByStartTime(id);
-    if (recordByStartTime.length === 0) return false;
+  async getRecordByStartTime(time) {
+    const recordByStartTime = await RecordRepository.getRecordByStartTime(time);
     return recordByStartTime;
   }
 
-  async getRecordByEndTime(id) {
-    const recordByEndTime = await RecordRepository.getRecordByEndTime(id);
-    if (recordByEndTime.length === 0) return false;
+  async getRecordByEndTime(time) {
+    const recordByEndTime = await RecordRepository.getRecordByEndTime(time);
     return recordByEndTime;
   }
 
