c4cd58e4833444ce1a7f7535372654e16c034a10
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-05-12 21:58:15 +0700

    #34 fix(back-end): fix mimetype of file in upload file

--- server-web/services/FileService.js
+++ server-web/services/FileService.js
@@ -13,7 +13,7 @@ let storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.includes("csv")) {
+  if (file.mimetype.includes("csv") || file.mimetype === 'text/plain' || file.mimetype === 'audio/wav') {
     console.log("valid");
     cb(null, true);
   } else {
