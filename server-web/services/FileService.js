97649a32b6740267d00a47e337e1d1e906fbc010
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-05-12 21:43:28 +0700

    #34 improve(back-end): add read, delete file record funtion

--- server-web/services/FileService.js
+++ server-web/services/FileService.js
@@ -1,10 +1,10 @@
-const multer = require('multer');
+const multer = require("multer");
 const maxSize = 2 * 1024 * 1024;
-const fs = require('fs');
+const fs = require("fs");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/upload/')
+    cb(null, "./public/upload/");
   },
   filename: (req, file, cb) => {
     console.log(file.originalname);
@@ -12,12 +12,13 @@ let storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req = req, file, cb) => {
-  if (file.mimetype === "text/plain") {
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.includes("csv")) {
     console.log("valid");
     cb(null, true);
   } else {
     console.log("invalid");
+    req.uploadFileError = true;
     cb(null, false);
   }
 };
@@ -27,25 +28,50 @@ class FileUploader {
     this.upload = multer({
       storage: storage,
       limits: { fileSize: maxSize },
-      fileFilter: fileFilter
-    }); 
+      fileFilter: fileFilter,
+    });
   }
 
   uploadFile(req, res, next) {
-    this.upload.single('file')(req, res, err => {
+    this.upload.single("file")(req, res, (err) => {
       if (err) {
-        return res.status(400).json({ error: err.message });
-      }
-     next();
-    }); 
+        return res.status(400).json({ message: "upload file error" });
+      } else if (req.uploadFileError) {
+        return res.status(500).json({
+          message: "wrong file format",
+        });
+      } else next();
+    });
   }
-
-  readFile(filename) {
-    const path = '/public/upload' + filename;
-
+  async readFile(path) {
+    let data = "";
+    return new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(path, { encoding: "utf8" });
+      readStream
+        .on("data", (chunk) => {
+          data = data.concat(" ", chunk);
+        })
+        .on("end", () => {
+          console.log("Finished reading");
+          resolve(data);
+        })
+        .on("error", (err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
   }
-
-
+  async deleteFile(path) {
+    return new Promise((resolve, reject) => {
+      fs.unlink(path, (err) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        }
+        resolve(true);
+      });
+    });
+  }
 }
 
 module.exports = new FileUploader();
