942af4d92e19027d2239618c31f3143fe0a42165
Huyz109 <phamqhuy2001@gmail.com>
2024-05-14 13:19:40 +0700

    improve(server-web): update user middleware

--- server-web/models/UserModel/UserRepository.js
+++ server-web/models/UserModel/UserRepository.js
@@ -37,9 +37,12 @@ class UserModel {
         account_id: user.account_id,
         username: user.username,
         birth: user.birth,
+        gender: user.gender,
         phone_number: user.phone_number ?? "",
         image: user.image ?? "",
         role: user.role,
+        created_at: Date.now(),
+        updated_at: Date.now(),
       },
       t && {
         transaction: t,
@@ -68,6 +71,7 @@ class UserModel {
         birth: user.birth,
         phone_number: user.phone_number,
         image: user.image,
+        updated_at: Date.now()
       },
       {
         where: {
@@ -79,6 +83,7 @@ class UserModel {
       }
     );
   }
+  
   async updateRoleById(id, role) {
     return await UserDTO.update(
       {
