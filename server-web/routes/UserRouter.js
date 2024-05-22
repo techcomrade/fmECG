99ce7fbd6f800f9ab5142150b3343520bd646ba4
hwt75 <dnthang300@gmail.com>
2024-05-22 15:46:16 +0700

    feature: issue #36 user role

--- server-web/routes/UserRouter.js
+++ server-web/routes/UserRouter.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
-const CommonMiddleware = require('../middlewares/CommonMiddleware');
+const {commonMiddleware} = require('../middlewares/CommonMiddleware');
 const UserMiddleware = require('../middlewares/UserMiddleware');
+const roleGroup = require
 const router = express.Router()
 
 router.post('', UserMiddleware.validateCreateData, UserController.createUser);
 router.post('/update', UserMiddleware.validateUpdateData, UserController.updateUser);
-router.get('', CommonMiddleware.validationToken, UserController.getAll);
+router.get('', commonMiddleware.validationToken, UserController.getAll);
 router.get('/:id', UserMiddleware.checkUserId , UserController.getUserById);
 router.delete('', UserMiddleware.checkUserId, UserController.deleteUser);
 
