d196b98a530a221c5a2f2f7ce86b18927a643cf0
thaidmfinnick <thai.dm279@gmail.com>
2024-05-24 11:45:31 +0700

    hide other folder

--- mobile_app/test/record_count_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test("test first", () {
-    int a = 3 + 2;
-    expect(a, 5);
-  });
-}
--- server-web/utils/processTime.js
+++
@@ -1,5 +0,0 @@
-const convertTimeToString = (timestamps) =>{
-    const date = new Date(timestamps)
-    return date.toString();
-}
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms*1000));
