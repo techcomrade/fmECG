ac335304909c2fbc9b8d7a799992f76ab8e8cf2e
CozDung29 <codung2909@gmail.com>
2024-05-26 14:48:30 +0700

    update (server): issue #57 get statistic UI

--- frontend_web/src/pages/Home/home.js
+++ frontend_web/src/pages/Home/home.js
@@ -1,10 +1,89 @@
 import './home.scss';
+import { Card, Statistic, Row, Col } from 'antd';
+import { DatabaseOutlined, UserOutlined, MobileOutlined } from '@ant-design/icons';
+
+import { useEffect, useState, useRef } from 'react';
+import { getStatistic } from '../../redux/reducer/statisticSlice';
+import { httpGetData } from '../../api/common.api';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Home = () => {
-    return (
-        <>
-            <h2 style={{textAlign: 'center', marginTop: '0'}}>Chào mừng bạn đến với web</h2>
-        </>
-    )
-}
+    const dispatch = useDispatch();
+    const dataState = useSelector((state) => state.statistic);
+    const [dropdownData, setDropData] = useState([]);
+
+    useEffect(() => {
+        dispatch(getStatistic(dataState));
+        const getOptionData = async() => {
+            const statisticData = await httpGetData("/statistic");
+            setDropData({
+                statistic: statisticData.metadata
+            }); 
+        }
+        getOptionData();
+    }, []);
+
+  return (
+    <>
+      <h2 style={{ textAlign: 'center', marginTop: '0' }}>Welcome to the website</h2>
+      <Row gutter={16} style={{ marginTop: '20px' }}>
+        <Col span={8}>
+          <Card
+            className="card-hover"
+            style={{
+              transition: 'box-shadow 0.3s ease-in-out',
+              '&:hover': {
+                boxShadow: '0 6px 16px 0 rgba(0, 0, 0, 0.2)',
+              },
+            }}
+          >
+            <Statistic
+              title={<span style={{ fontSize: '16px', fontWeight: 'bold' }}>Total Records</span>}
+              value={dropdownData.statistic?.record_count}
+              valueStyle={{ color: '#000000', fontSize: '24px', fontWeight: 'bold' }}
+              prefix={<DatabaseOutlined style={{ fontSize: '24px', marginRight: '8px' }} />}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            className="card-hover"
+            style={{
+              transition: 'box-shadow 0.3s ease-in-out',
+              '&:hover': {
+                boxShadow: '0 6px 16px 0 rgba(0, 0, 0, 0.2)',
+              },
+            }}
+          >
+            <Statistic
+              title={<span style={{ fontSize: '16px', fontWeight: 'bold' }}>Users</span>}
+              value={dropdownData.statistic?.user_count}
+              valueStyle={{ color: '#000000', fontSize: '24px', fontWeight: 'bold' }}
+              prefix={<UserOutlined style={{ fontSize: '24px', marginRight: '8px' }} />}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            className="card-hover"
+            style={{
+              transition: 'box-shadow 0.3s ease-in-out',
+              '&:hover': {
+                boxShadow: '0 6px 16px 0 rgba(0, 0, 0, 0.2)',
+              },
+            }}
+          >
+            <Statistic
+              title={<span style={{ fontSize: '16px', fontWeight: 'bold' }}>Devices</span>}
+              value={dropdownData.statistic?.device_count}
+              valueStyle={{ color: '#000000', fontSize: '24px', fontWeight: 'bold' }}
+              prefix={<MobileOutlined style={{ fontSize: '24px', marginRight: '8px' }} />}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
 export default Home;
---
+++ frontend_web/src/redux/reducer/statisticSlice.js
@@ -0,0 +1,60 @@
+import { httpGetData } from "../../api/common.api";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadStatus = {
+    None: 0,
+    Loading: 1,
+    Success: 2,
+    Failed: 3,
+};
+
+export const getStatistic = createAsyncThunk(
+    "/statistic",
+    async (params, { rejectWithValue }) => {
+      try {
+        const response = await httpGetData('/statistic');
+        return response;
+      } catch (error) {
+        return rejectWithValue(
+          error?.response?.data?.message || error?.response || error
+        );
+      }
+    }
+  );
+
+const statisticSlice = createSlice({
+    name: 'statistic',
+    initialState: {
+      data: [],
+      loadDataStatus: loadStatus.None,
+      loadCreateDataStatus: loadStatus.None,
+      loadUpdateDataStatus: loadStatus.None,
+      loadDeleteDataStatus: loadStatus.None,
+    },
+    reducers: {
+      resetLoadDataStatus: (state, action) => {
+        state.data = [];
+        state.loadDataStatus = loadStatus.None;
+      },
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(getStatistic.pending, (state, action) => {
+          state.loadDataStatus = loadStatus.Loading;
+        })
+        .addCase(getStatistic.fulfilled, (state, action) => {
+          state.data = action.payload;
+          state.loadDataStatus = loadStatus.Success;
+        })
+        .addCase(getStatistic.rejected, (state, action) => {
+          state.data = [];
+          state.loadDataStatus = loadStatus.Failed;
+        })
+    }
+});
+
+const { reducer: statisticReducer } = statisticSlice;
+
+export const { resetLoadDataStatus } = statisticSlice.actions;
+
+export default statisticReducer;
