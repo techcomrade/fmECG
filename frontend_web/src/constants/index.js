e371807281d245678442724b993a3cc3a4c29b21
Huyz109 <phamqhuy2001@gmail.com>
2024-05-31 15:46:17 +0700

    feature(front-end): add device status

--- frontend_web/src/constants/index.js
+++ frontend_web/src/constants/index.js
@@ -28,7 +28,8 @@ export const userRole = {
   admin: '0',
   doctor: '1',
   patient: '2'
-}
+};
+
 export const UserStatus = [
   {
     value: 0,
@@ -38,7 +39,23 @@ export const UserStatus = [
     value: 1,
     label: "Đã nghỉ"
   }
-]
+];
+
+export const DeviceStatus = [
+  {
+    value: 0,
+    label: "Đang hoạt động"
+  },
+  {
+    value: 1,
+    label: "Đang trống"
+  },
+  {
+    value: 2,
+    label: "Đang bảo trì"
+  }
+];
+
 export const convertGenderToString = (gender) => {
   switch (gender) {
     case 0:
@@ -49,6 +66,7 @@ export const convertGenderToString = (gender) => {
       return "Không rõ";
   }
 };
+
 export const convertStringToGender = (gender) => {
   switch (gender) {
     case "Nam":
@@ -92,5 +110,31 @@ export const convertStatusToString =  (status) => {
       return "Đã nghỉ";
     default:
       return "Không xác định";
+  }
+}
+
+export const convertDeviceStatusToString =  (status) => {
+  switch (status){
+    case 0:
+      return "Đang hoạt đông";
+    case 1:
+      return "Đang trống";
+    case 2:
+      return "Đang bảo trì";
+    default:
+      return "Không xác định";
+  }
+}
+
+export const convertDeviceStatusColor =  (status) => {
+  switch (status){
+    case 0:
+      return "geekblue";
+    case 1:
+      return "green";
+    case 2:
+      return "volcano";
+    default:
+      return "red";
   }
 }
