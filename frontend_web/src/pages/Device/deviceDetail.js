035fcff7d9cd964e5f094409a71b3cbce5ac7c7e
Huyz109 <phamqhuy2001@gmail.com>
2024-06-02 21:16:50 +0700

    improve(front-end): update device detail

--- frontend_web/src/pages/Device/deviceDetail.js
+++ frontend_web/src/pages/Device/deviceDetail.js
@@ -4,7 +4,8 @@ import { convertTimeToDate } from "../../utils/dateUtils";
 import { DrawerSide } from '../../components/Drawer/Drawer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDeviceById, loadStatus } from '../../redux/reducer/deviceSlice';
-import { Divider } from 'antd';
+import { Divider, Table } from 'antd';
+import { convertDeviceStatusToString } from '../../constants';
 
 const DeviceDetailComponent = (props, ref) => {
   const dispatch = useDispatch();
@@ -15,12 +16,16 @@ const DeviceDetailComponent = (props, ref) => {
   const { deviceData, loadDeviceDataStatus } = useSelector((state) => state.device);
 
   const handleData = (data) => {
-    const deviceData = {...data};
-      Object.keys(data).forEach((key) => {
-        if (checkDateTypeKey(key)) {
-          deviceData[key] = convertTimeToDate(data[key]);
-        }
-      })
+    const deviceData = {
+      ...data,
+      status: convertDeviceStatusToString(data.status)
+    };
+
+    Object.keys(data).forEach((key) => {
+      if (checkDateTypeKey(key)) {
+        deviceData[key] = convertTimeToDate(data[key]);
+      }
+    })
 
     return deviceData;
   };
@@ -45,10 +50,39 @@ const DeviceDetailComponent = (props, ref) => {
   const labelsInfo = {
     device_name: 'Tên thiết bị',
     device_type: 'Loại thiết bị',
-    end_date: "Ngày kết thúc",
-    start_date: "Ngày bắt đầu",
+    frequency: "Tần số",
     recordCount: "Số bản ghi",
+    status: "Trạng thái",
+    start_date: "Ngày bắt đầu",
   }; 
+
+  const frequencyColumns = [
+    {
+      title: 'Thông tin',
+      dataIndex: 'information',
+      key: 'information',
+    },
+    {
+      title: 'Giá trị',
+      dataIndex: 'value',
+      key: 'value',
+    },
+    {
+      title: 'Đơn vị',
+      dataIndex: 'frequency_name',
+      key: 'frequency_name',
+    },
+  ]
+
+  const customDetail = {
+    frequency: (
+      <Table 
+        columns={frequencyColumns} 
+        dataSource={data.frequency} 
+        pagination={false}
+      />
+    )
+  }
 
   const customData = (
     <>
@@ -67,6 +101,7 @@ const DeviceDetailComponent = (props, ref) => {
         data = {data}
         labels = {labelsInfo}
         customData={customData}
+        customDetail={customDetail}
        />
     </>
   );
