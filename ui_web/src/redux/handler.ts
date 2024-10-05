42763db6bba3c7b294d65706d0f7dde9805af373
hwt75 <dnthang300@gmail.com>
2024-10-05 15:02:41 +0700

    t20241#15: update config redux/authenSlice.ts

--- ui_web/src/redux/handler.ts
+++ ui_web/src/redux/handler.ts
@@ -1,5 +1,32 @@
+
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const createAsyncThunkWrap = ()=> {
-    
+export interface IExceptionModel {
+  requestId: string;
+  timestamp: string;
+  statusCode: number;
+  errorCode: string;
 }
+
+export const createAsyncThunkWrap = <TReturn, TData = void>(
+  key: string,
+  action: (param: TData) => Promise<TReturn>
+) => {
+  return createAsyncThunk(key, async (thunkArg: TData, { rejectWithValue }) => {
+    try {
+      window.localStorage.setItem("context.lastActivityTime", Date.now().toString());
+      return await action(thunkArg);
+    } catch (e: any) {
+      // todo: common error handler, eg. 401
+      const exceptionModel: IExceptionModel = JSON.parse(e.response);
+
+      if (exceptionModel.statusCode === 401) {
+        const event:any = new Event("session-timeout");
+        event.key= "session-timeout";
+        window.dispatchEvent(event);
+      }
+
+      return rejectWithValue(exceptionModel);
+    }
+  });
+};
