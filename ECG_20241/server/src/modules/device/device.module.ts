2fa674ac2ca90dae7a3fcdc8d9456336ae2ff853
CozDung29 <codung2909@gmail.com>
2024-10-08 23:17:35 +0700

    merged

--- ECG_20241/server/src/modules/device/device.module.ts
+++ ECG_20241/server/src/modules/device/device.module.ts
@@ -4,10 +4,12 @@ import { DeviceController } from './device.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { DeviceService } from './device.service';
 import { DeviceModel } from '../../entities/device.model';
+import { DeviceStatusModel } from '../../entities/device_status.model';
+import { DeviceTypeModel } from '../../entities/device_type.model';
 
 @Module({
     imports: [
-        SequelizeModule.forFeature([DeviceModel]),
+        SequelizeModule.forFeature([DeviceModel, DeviceStatusModel, DeviceTypeModel]),
     ],
     controllers: [DeviceController],
     providers: [DeviceService, DeviceRepository],
