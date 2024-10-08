1c6cb19b63688388157bf56b379af3821f59fc3b
CozDung29 <codung2909@gmail.com>
2024-10-08 23:27:43 +0700

    Save my work

--- ECG_20241/server/src/modules/user/user.module.ts
+++ ECG_20241/server/src/modules/user/user.module.ts
@@ -6,12 +6,21 @@ import { UserService } from './user.service';
 import { UserModel } from '../../entities/user.model';
 import { TokenModule } from '../token/token.module';
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import { UserStatusModel }  from '../../entities/user_status.model';
 import { UserRoleModel } from '../../entities/user_role.model';
 
 @Module({
     imports: [
         SequelizeModule.forFeature([UserModel,UserStatusModel, UserRoleModel]),
+=======
+import { UserRoleModel } from '../../entities/user_role.model';
+import { UserStatusModel } from '../../entities/user_status.model';
+
+@Module({
+    imports: [
+        SequelizeModule.forFeature([UserModel, UserRoleModel, UserStatusModel]),
+>>>>>>> Stashed changes
 =======
 import { UserRoleModel } from '../../entities/user_role.model';
 import { UserStatusModel } from '../../entities/user_status.model';
