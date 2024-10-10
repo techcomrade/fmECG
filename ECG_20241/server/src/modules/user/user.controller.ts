ec8860a669f032646dec1fc7b05927f518532294
Duong <duongtoan1k99@gmail.com>
2024-10-10 09:18:27 +0700

    improve(back-end): update token and user module

--- ECG_20241/server/src/modules/user/user.controller.ts
+++ ECG_20241/server/src/modules/user/user.controller.ts
@@ -1,27 +1,24 @@
-import { AuthenticationGuard } from '../common/guards/authentication.guard';
-import { Controller, Get, Post, Body,  UseGuards } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserModel } from '../../entities/user.model';
-import { Roles } from '../common/roles/role.decorator';
-import { Role } from '../common/roles/role.enum';
-import { AuthorizationGuard } from '../common/guards/authorization.guard';
+import { AuthenticationGuard } from "../common/guards/authentication.guard";
+import { Controller, Get, Post, Body, UseGuards } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserModel } from "../../entities/user.model";
+import { Roles } from "../common/roles/role.decorator";
+import { Role } from "../common/roles/role.enum";
+import { AuthorizationGuard } from "../common/guards/authorization.guard";
 
 @Controller("users")
 export class UserController {
-    constructor(
-        private userService: UserService
-    ){}
+  constructor(private userService: UserService) {}
 
-    @UseGuards(AuthenticationGuard, AuthorizationGuard)
-    @Roles(Role.Admin)
-    @Get('')
-    async findAll(){
-        return await this.userService.findAll();
-    }
+  @UseGuards(AuthenticationGuard, AuthorizationGuard)
+  @Roles(Role.Admin)
+  @Get("")
+  async findAll() {
+    return await this.userService.findAll();
+  }
 
-    @Post('register')
-    async add(@Body() User: UserModel){
-        return await this.userService.add(User);
-    }
-
+  @Post("")
+  async add(@Body() User: UserModel) {
+    return await this.userService.add(User);
+  }
 }
--- ECG_20241/server/src/modules/user/user.module.ts
+++ ECG_20241/server/src/modules/user/user.module.ts
@@ -1,19 +1,20 @@
-import { UserRepository } from './model/user.repository';
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UserService } from './user.service';
-import { UserModel } from '../../entities/user.model';
-import { TokenModule } from '../token/token.module';
+import { UserRepository } from "./user.repository";
+import { Module } from "@nestjs/common";
+import { UserController } from "./user.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UserService } from "./user.service";
+import { UserModel } from "../../entities/user.model";
+import { TokenModule } from "../token/token.module";
+import { UserRoleModel } from "../../entities/user_role.model";
+import { UserStatusModel } from "../../entities/user_status.model";
 
 @Module({
-    imports: [
-        SequelizeModule.forFeature([UserModel]),
-        TokenModule
-    ],
-    controllers: [UserController],
-    providers: [UserService, UserRepository],
-    exports: [UserService]
+  imports: [
+    SequelizeModule.forFeature([UserModel, UserRoleModel, UserStatusModel]),
+    TokenModule,
+  ],
+  controllers: [UserController],
+  providers: [UserService, UserRepository],
+  exports: [UserService],
 })
-
 export class UserModule {}
