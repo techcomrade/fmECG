51e570b1a762a623489656180f64f4c0201896f0
hwt75 <dnthang300@gmail.com>
2024-10-10 14:30:11 +0700

    t20241#13: define database

--- identity_service/src/app.module.ts
+++ identity_service/src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Load file cấu hình database
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql', // Kiểu cơ sở dữ liệu (MySQL)
+        host: configService.get<string>('DB_HOST'), // Lấy host từ biến môi trường
+        port: configService.get<number>('DB_PORT'), // Port dạng số
+        username: configService.get<string>('DB_USER'), // Tên người dùng MySQL
+        password: configService.get<string>('DB_PASSWORD'), // Mật khẩu MySQL
+        database: configService.get<string>('DB_NAME'), // Tên cơ sở dữ liệu
+        models: [], // Đăng ký model Sequelize
+        autoLoadModels: true, // Tự động tạo bảng nếu chưa có
+        synchronize: true, // Đồng bộ hóa model với cơ sở dữ liệu
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
