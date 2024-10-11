fb3e338bd89f3c58795ceb171626cff8c9b3d008
CozDung29 <codung2909@gmail.com>
2024-10-11 21:11:00 +0700

    [improve] : t20241#16 seed data

---
+++ ECG_20241/server/seeders/20241011113640-dummy-device-type.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("device_type", [
+      {
+        id: 1,
+        type_name: "hard",
+        createdAt: "2024-10-09T16:51:43.000",
+        updatedAt: "2024-10-09T16:51:43.000",
+      },
+      {
+        id: 2,
+        type_name: "soft",
+        createdAt: "2024-10-09T16:51:43.000",
+        updatedAt: "2024-10-09T16:51:43.000",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("device_type", null, {});
+  },
+};
