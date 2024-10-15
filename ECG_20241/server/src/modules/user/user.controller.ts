93190553b7b77c5f7fbe061ce47cff303b56460a
Duong <duongtoan1k99@gmail.com>
2024-10-15 13:46:41 +0700

    improve(back-end): enable cors, fix get all user api

--- ECG_20241/server/src/modules/user/user.controller.ts
+++ ECG_20241/server/src/modules/user/user.controller.ts
@@ -1,43 +1,58 @@
 import { AuthenticationGuard } from "../common/guards/authentication.guard";
-import { Controller, Get, Post, Body, UseGuards, Query, Res, BadRequestException, HttpStatus, NotFoundException } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Query,
+  Res,
+  BadRequestException,
+  HttpStatus,
+  NotFoundException,
+} from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserModel } from "../../entities/user.model";
 import { Roles } from "../common/roles/role.decorator";
 import { Role } from "../common/roles/role.enum";
 import { AuthorizationGuard } from "../common/guards/authorization.guard";
-import { Response } from 'express';
+import { Response } from "express";
+import { ApiResponse } from "@nestjs/swagger";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
   constructor(private userService: UserService) {}
 
   // @UseGuards(AuthenticationGuard, AuthorizationGuard)
   // @Roles(Role.Admin)
-  @Get('')
-  async findAll() {
+  @Get("")
+  @ApiResponse({ status: 200, type: [UserModel], description: "successful" })
+  async findAll(): Promise<UserModel[]> {
     return await this.userService.findAll();
   }
 
-  @Post('')
+  @Post("")
   async add(@Body() User: UserModel) {
     return await this.userService.add(User);
   }
 
-  @Get('details')
-  async findByUserName(@Res() res: Response, @Query('username') username: string){
-    console.log(`[P]:::Find user by username: `, username)
-    if(!username){
+  @Get("details")
+  async findByUserName(
+    @Res() res: Response,
+    @Query("username") username: string
+  ) {
+    console.log(`[P]:::Find user by username: `, username);
+    if (!username) {
       throw new BadRequestException("username is required");
     }
     try {
       const users = await this.userService.findByUserName(username);
       return res.status(HttpStatus.OK).json({
-        message: 'User found',
-        metadata: users
+        message: "User found",
+        metadata: users,
       });
-    }
-    catch (err) {
-      throw new NotFoundException('No user found, please try again');
+    } catch (err) {
+      throw new NotFoundException("No user found, please try again");
     }
   }
 }
