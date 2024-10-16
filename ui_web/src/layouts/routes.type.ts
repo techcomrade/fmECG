d9b78e85a87dc122dbb4a0492a52ac3eeaa867fb
Duong <duongtoan1k99@gmail.com>
2024-10-16 19:53:20 +0700

    feature(front-end): add device router path

--- ui_web/src/layouts/routes.type.ts
+++ ui_web/src/layouts/routes.type.ts
@@ -1,60 +1,60 @@
-
 export interface IRouteItem {
-    key: string;
-    i18nKey?: string;
-    url: string;
-    iconName?: string;
-    parentKeys?: string[],
-    specificBeadTextKey?: string
+  key: string;
+  i18nKey?: string;
+  url: string;
+  iconName?: string;
+  parentKeys?: string[];
+  specificBeadTextKey?: string;
 }
 export const getPage = () => {
-    return "Page";
-} 
+  return "Page";
+};
 
-export const routeMapping: { [key: string]: IRouteItem} = {
-    Home: {
-        key: "home",
-        url: "/home"
-    },
-    User: {
-        key: "user",
-        url: "/user"
-    },
-    ErrorPage: {
-      key: "error",
-      url: "/error"
-    },
-    LoginFailed: {
-      key: "login-failed",
-      url: "/login-failed"
-    },
-    Login: {
-      key: "login",
-      url: "/login"
-    }
-}
+export const routeMapping: { [key: string]: IRouteItem } = {
+  Home: {
+    key: "home",
+    url: "/home",
+  },
+  User: {
+    key: "user",
+    url: "/user",
+  },
+  Device: {
+    key: "device",
+    url: "/device",
+  },
+  ErrorPage: {
+    key: "error",
+    url: "/error",
+  },
+  LoginFailed: {
+    key: "login-failed",
+    url: "/login-failed",
+  },
+  Login: {
+    key: "login",
+    url: "/login",
+  },
+};
 
 export const getRouteItemByKey = (routeKey: string) => {
-    let currentRouteItem = routeMapping.Home;
-    for (const key in routeMapping) {
-      const item = routeMapping[key];
-      if (item.key === routeKey) {
-        currentRouteItem = item;
-      }
+  let currentRouteItem = routeMapping.Home;
+  for (const key in routeMapping) {
+    const item = routeMapping[key];
+    if (item.key === routeKey) {
+      currentRouteItem = item;
     }
-    return currentRouteItem;
-  };
+  }
+  return currentRouteItem;
+};
 
-
-  
 export const getRouteItemByUrl = (url: string) => {
-    let currentRouteItem = routeMapping.Home;
-    for (const key in routeMapping) {
-      const item = routeMapping[key];
-      if (item.url === url) {
-        currentRouteItem = item;
-      }
+  let currentRouteItem = routeMapping.Home;
+  for (const key in routeMapping) {
+    const item = routeMapping[key];
+    if (item.url === url) {
+      currentRouteItem = item;
     }
-    return currentRouteItem;
-  };
-  
+  }
+  return currentRouteItem;
+};
