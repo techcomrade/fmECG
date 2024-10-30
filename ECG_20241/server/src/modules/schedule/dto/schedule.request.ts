2b8d8789d91e49200fdd244ea726191b29d53db8
Duong <duongtoan1k99@gmail.com>
2024-10-30 10:52:51 +0700

    improve(back-end): add create consultation schedule in create schedule by doctor API

--- ECG_20241/server/src/modules/schedule/dto/schedule.request.ts
+++ ECG_20241/server/src/modules/schedule/dto/schedule.request.ts
@@ -1,25 +1,39 @@
-import { IsNotEmpty, IsString, IsInt, IsOptional, IsDateString, IsNumber, IsUUID } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsInt,
+  IsNumber,
+  IsUUID,
+} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class ScheduleRequest {
   @ApiProperty({
-    description: 'Unique identifier for the schedule',
-    example: 'abcd1234-ff2e-43b0-8f78-4dcc98128a16',
+    description: "Unique identifier for the schedule",
+    example: "abcd1234-ff2e-43b0-8f78-4dcc98128a16",
   })
   @IsNotEmpty()
   @IsUUID()
   id?: string;
 
   @ApiProperty({
-    description: 'Unique identifier for the patient',
-    example: 'f4289c39-ff2e-43b0-8f78-4dcc98128a16',
+    description: "Unique identifier for the patient",
+    example: "f4289c39-ff2e-43b0-8f78-4dcc98128a16",
   })
   @IsNotEmpty()
   @IsString()
   patient_id: string;
 
   @ApiProperty({
-    description: 'Start time of the schedule (in Unix timestamp format)',
+    description: "Unique identifier for the account of the doctor",
+    example: "f4289c39-ff2e-43b0-8f78-4dcc98128a16",
+  })
+  @IsNotEmpty()
+  @IsString()
+  account_id?: string;
+
+  @ApiProperty({
+    description: "Start time of the schedule (in Unix timestamp format)",
     example: 1697011200,
   })
   @IsNotEmpty()
@@ -27,7 +41,7 @@ export class ScheduleRequest {
   schedule_start_time: bigint;
 
   @ApiProperty({
-    description: 'End time of the schedule (in Unix timestamp format)',
+    description: "End time of the schedule (in Unix timestamp format)",
     example: 1697014800,
   })
   @IsNotEmpty()
@@ -35,7 +49,7 @@ export class ScheduleRequest {
   schedule_end_time: bigint;
 
   @ApiProperty({
-    description: 'Type ID of the schedule',
+    description: "Type ID of the schedule",
     example: 1,
   })
   @IsNotEmpty()
@@ -43,10 +57,9 @@ export class ScheduleRequest {
   schedule_type_id: number;
 
   @ApiProperty({
-    description: 'Status ID of the schedule',
+    description: "Status ID of the schedule",
     example: 1,
   })
-  @IsNotEmpty()
   @IsInt()
-  status_id: number;
+  status_id?: number;
 }
