68acd68969aa5f0d76d6c8a63386c888a4904b65
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2024-10-31 13:24:26 +0700

    improve(back-end): edit new api get schedule with available time of doctor

--- ECG_20241/server/src/modules/schedule/schedule.controller.ts
+++ ECG_20241/server/src/modules/schedule/schedule.controller.ts
@@ -1,9 +1,24 @@
 import { AuthenticationGuard } from "../common/guards/authentication.guard";
-import { Controller, Get, Post, Body, UseGuards, Query, Res, BadRequestException, HttpStatus, NotFoundException, Delete, Put, InternalServerErrorException } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Query,
+  Res,
+  BadRequestException,
+  HttpStatus,
+  NotFoundException,
+  Delete,
+  Put,
+  InternalServerErrorException,
+  Param,
+} from "@nestjs/common";
 import { Roles } from "../common/roles/role.decorator";
 import { Role } from "../common/roles/role.enum";
 import { AuthorizationGuard } from "../common/guards/authorization.guard";
-import { Response } from 'express';
+import { Response } from "express";
 import { ApiResponse } from "@nestjs/swagger";
 
 import { ScheduleResponse } from "./dto/schedule.response";
@@ -13,13 +28,13 @@ import { plainToInstance } from "class-transformer";
 import { UserService } from "../user/user.service";
 import { ConsultationScheduleService } from "../consultation_schedule/consultation_schedule.service";
 
-@Controller('schedules')
+@Controller("schedules")
 export class ScheduleController {
   constructor(
     private scheduleService: ScheduleService,
     private userService: UserService,
     private consultationScheduleService: ConsultationScheduleService
-  ) { }
+  ) {}
 
   // @UseGuards(AuthenticationGuard, AuthorizationGuard)
   // @Roles(Role.Admin)
@@ -27,7 +42,7 @@ export class ScheduleController {
   @ApiResponse({
     status: 200,
     type: [ScheduleResponse],
-    description: "successful"
+    description: "successful",
   })
   async getAllSchedules(@Res() res: Response) {
     console.log(`[P]:::Get all schedule data`);
@@ -48,11 +63,11 @@ export class ScheduleController {
   @ApiResponse({
     status: 200,
     type: [ScheduleResponse],
-    description: "successful"
+    description: "successful",
   })
   async getScheduleByPatientId(
     @Res() res: Response,
-    @Query('patient_id') patient_id: string
+    @Query("patient_id") patient_id: string
   ) {
     console.log(`[P]:::Get schedule by patient id`, patient_id);
     let checkExistPatient = await this.userService.getUserById(patient_id);
@@ -60,7 +75,9 @@ export class ScheduleController {
       throw new NotFoundException("No patient found, please try again");
     }
     try {
-      let schedules = await this.scheduleService.getScheduleByPatientId(patient_id);
+      let schedules = await this.scheduleService.getScheduleByPatientId(
+        patient_id
+      );
       if (!schedules.length) {
         throw new NotFoundException("No schedule found, please try again");
       }
@@ -68,7 +85,9 @@ export class ScheduleController {
       return res.json(result);
     } catch (error) {
       console.log(error);
-      throw new InternalServerErrorException("Error when get schedule by patient id");
+      throw new InternalServerErrorException(
+        "Error when get schedule by patient id"
+      );
     }
   }
 
@@ -76,11 +95,11 @@ export class ScheduleController {
   @ApiResponse({
     status: 200,
     type: [ScheduleResponse],
-    description: "successful"
+    description: "successful",
   })
   async getScheduleByDoctorId(
     @Res() res: Response,
-    @Query('doctor_id') doctor_id: string
+    @Query("doctor_id") doctor_id: string
   ) {
     console.log(`[P]:::Get schedule by doctor_id id`, doctor_id);
     let checkExistDoctor = await this.userService.getUserById(doctor_id);
@@ -88,7 +107,9 @@ export class ScheduleController {
       throw new NotFoundException("No doctor found, please try again");
     }
     try {
-      let schedules = await this.scheduleService.getScheduleByDoctorId(doctor_id);
+      let schedules = await this.scheduleService.getScheduleByDoctorId(
+        doctor_id
+      );
       if (!schedules.length) {
         throw new NotFoundException("No schedule found, please try again");
       }
@@ -96,7 +117,9 @@ export class ScheduleController {
       return res.json(result);
     } catch (error) {
       console.log(error);
-      throw new InternalServerErrorException("Error when get schedule by doctor id");
+      throw new InternalServerErrorException(
+        "Error when get schedule by doctor id"
+      );
     }
   }
 
@@ -106,7 +129,10 @@ export class ScheduleController {
     type: Boolean,
     description: "Successful",
   })
-  async createSchedule(@Body() schedule: ScheduleRequest, @Res() res: Response) {
+  async createSchedule(
+    @Body() schedule: ScheduleRequest,
+    @Res() res: Response
+  ) {
     console.log(`[P]:::Create schedule data`, schedule);
     try {
       await this.scheduleService.createSchedule(schedule);
@@ -125,11 +151,18 @@ export class ScheduleController {
     type: Boolean,
     description: "Successful",
   })
-  async updateScheduleById(@Body() schedule: ScheduleRequest, @Res() res: Response) {
+  async updateScheduleById(
+    @Body() schedule: ScheduleRequest,
+    @Res() res: Response
+  ) {
     console.log(`[P]:::Update schedule by id`, schedule.id);
-    let checkExistSchedule = await this.scheduleService.getScheduleById(schedule.id);
+    let checkExistSchedule = await this.scheduleService.getScheduleById(
+      schedule.id
+    );
     if (checkExistSchedule == null) {
-      throw new NotFoundException("No schedule found to update, please try again");
+      throw new NotFoundException(
+        "No schedule found to update, please try again"
+      );
     }
     try {
       await this.scheduleService.updateSchedule(schedule, schedule.id);
@@ -148,11 +181,13 @@ export class ScheduleController {
     type: Boolean,
     description: "Successful",
   })
-  async deleteScheduleById(@Res() res: Response, @Query('id') id: string) {
+  async deleteScheduleById(@Res() res: Response, @Query("id") id: string) {
     console.log(`[P]:::Delete schedule by id`, id);
     let checkExistSchedule = await this.scheduleService.getScheduleById(id);
     if (checkExistSchedule == null) {
-      throw new NotFoundException("No schedule found to delete, please try again");
+      throw new NotFoundException(
+        "No schedule found to delete, please try again"
+      );
     }
     try {
       await this.scheduleService.deleteScheduleById(id);
@@ -164,5 +199,70 @@ export class ScheduleController {
     }
   }
 
+  @Get("/doctor/:id")
+  @ApiResponse({
+    status: 200,
+    type: [ScheduleResponse],
+    description: "Successfully",
+  })
+  async getAvailableScheduleByDoctorId(
+    @Res() res: Response,
+    @Param("id") id: string
+  ) {
+    console.log(`[P]:::Get available schedules of doctor by doctor id`, id);
+    const currentDate = new Date();
+    const timestamp = Math.floor(currentDate.getTime() / 1000);
 
+    console.log(timestamp);
+    try {
+      const scheduleList =
+        await this.scheduleService.getScheduleByDoctorIdWithTime(id, timestamp);
+      if (scheduleList.length <= 0)
+        return res.json({
+          message: "This doctor has no busy time",
+        });
+      const availableSchedule = [];
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+
+      for (let i = 0; i < 14; i++) {
+        const date = new Date(today);
+        date.setDate(today.getDate() + i);
+        const hours = [
+          8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15,
+          15.5, 16, 16.5, 17, 17.5,
+        ];
+
+        // const filterHours = hours.filter((hour) => {
+        //   const hourStart = new Date(date).setHours(
+        //     Math.floor(hour),
+        //     (hour % 1) * 60,
+        //     0,
+        //     0
+        //   );
+        //   return !scheduleList.some(
+        //     (schedule) => hourStart === schedule.schedule_start_time
+        //   );
+        // });
+
+        const dayOfWeek = date.getDay();
+        if (dayOfWeek !== 0 && dayOfWeek !== 6) {
+          availableSchedule.push({
+            date,
+            hours: [
+              8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5,
+              15, 15.5, 16, 16.5, 17, 17.5,
+            ],
+          });
+        }
+      }
+      console.log(availableSchedule);
+      return res.json(scheduleList);
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException(
+        "Error when getting available schedules of doctor"
+      );
+    }
+  }
 }
