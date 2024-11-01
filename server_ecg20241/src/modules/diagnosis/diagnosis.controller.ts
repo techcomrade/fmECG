abc40af0df2ba8762675f637a75e4c680b789527
Duong <duongtoan1k99@gmail.com>
2024-11-01 17:31:07 +0700

    feature(back-end): add get diagnosis by schedule id API

--- server_ecg20241/src/modules/diagnosis/diagnosis.controller.ts
+++ server_ecg20241/src/modules/diagnosis/diagnosis.controller.ts
@@ -4,11 +4,17 @@ import {
   Body,
   Res,
   BadRequestException,
+  Get,
+  Param,
+  NotFoundException,
+  InternalServerErrorException,
 } from "@nestjs/common";
 import { Response } from "express";
 import { ApiResponse } from "@nestjs/swagger";
 import { DiagnosisService } from "./diagnosis.service";
 import { DiagnosisRequest } from "./dto/diagnosis.request";
+import { DiagnosisResponse } from "./dto/diagnosis.response";
+import { plainToInstance } from "class-transformer";
 
 @Controller("diagnosis")
 export class DiagnosisController {
@@ -24,7 +30,7 @@ export class DiagnosisController {
     @Body() diagnosis: DiagnosisRequest,
     @Res() res: Response
   ) {
-    console.log(`[P]:::Create diagnosis data`, diagnosis);
+    console.log("[P]:::Create diagnosis data", diagnosis);
     try {
       await this.diagnosisService.createDiagnosis(diagnosis);
       return res.json({
@@ -33,6 +39,34 @@ export class DiagnosisController {
     } catch (error) {
       console.log(error);
       throw new BadRequestException("Failed to create diagnosis");
+    }
+  }
+
+  @Get("schedule/:schedule_id")
+  @ApiResponse({
+    status: 200,
+    type: DiagnosisResponse,
+    description: "successful",
+  })
+  async getDiagnosisByScheduleId(
+    @Res() res: Response,
+    @Param("schedule_id") schedule_id: string
+  ) {
+    console.log("[P]:::Get diagnosis by schedule id", schedule_id);
+    try {
+      let diagnosis = await this.diagnosisService.getDiagnosisByScheduleId(
+        schedule_id
+      );
+      if (!diagnosis) {
+        throw new NotFoundException("No diagnosis found, please try again");
+      }
+      let result = plainToInstance(DiagnosisResponse, diagnosis);
+      return res.json(result);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(
+        "Error when get diagnosis by schedule id"
+      );
     }
   }
 }
--- server_ecg20241/src/modules/diagnosis/diagnosis.repository.ts
+++ server_ecg20241/src/modules/diagnosis/diagnosis.repository.ts
@@ -2,6 +2,7 @@ import { ConflictException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { DiagnosisModel } from "../../entities/diagnosis.model";
 import { DiagnosisRequest } from "./dto/diagnosis.request";
+import { DiagnosisResponse } from "./dto/diagnosis.response";
 const { v4: uuidv4 } = require("uuid");
 
 @Injectable()
@@ -16,6 +17,16 @@ export class DiagnosisRepository {
       id: diagnosis.id,
       schedule_id: diagnosis.schedule_id,
       information: diagnosis.information,
+    });
+  }
+
+  async getDiagnosisByScheduleId(
+    schedule_id: string
+  ): Promise<DiagnosisResponse> {
+    return await this.diagnosisModel.findOne({
+      where: {
+        schedule_id: schedule_id,
+      },
     });
   }
 }
