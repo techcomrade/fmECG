97c52fedc5d6bb3a650a0555f604411434ad0ae2
Duong <duongtoan1k99@gmail.com>
2024-11-01 22:51:54 +0700

    fix(back-end): fix data fields related to datetime

--- server_ecg20241/seeders/20241011114006-dummy-schedules.js
+++ server_ecg20241/seeders/20241011114006-dummy-schedules.js
@@ -7,8 +7,8 @@ module.exports = {
       {
         id: "9b2a5f71-23b7-47c8-b9b5-4a7d5e5f6d3b",
         patient_id: "bc372cb1-2e6f-4727-b2ad-bd041bd9fbb5",
-        schedule_start_time: 1727800000,
-        schedule_end_time: 1727800600,
+        schedule_start_time: 1704958200,
+        schedule_end_time: 1704960000,
         schedule_type_id: 2,
         status_id: 1,
         createdAt: "2024-10-12T08:45:33.000",
@@ -17,8 +17,8 @@ module.exports = {
       {
         id: "6c28faba-d431-41db-a5a9-2dcb5fe2a241",
         patient_id: "ecb4b5a1-58f6-4b33-bc26-d8bca0c2f22e",
-        schedule_start_time: 1727801000,
-        schedule_end_time: 1727801600,
+        schedule_start_time: 1730250000,
+        schedule_end_time: 1730251800,
         schedule_type_id: 1,
         status_id: 1,
         createdAt: "2024-10-12T09:10:12.000",
