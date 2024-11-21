889abf0080b3087b64a9dc9cddb4bb70d839e23d
Duong <duongtoan1k99@gmail.com>
2024-11-21 13:32:10 +0700

    improve(identity_service): add ValidationPipe

--- identity_service/src/main.ts
+++ identity_service/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 const port = process.env.APP_PORT || 3001;
 const host = process.env.APP_HOST || 'localhost';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('My API')
