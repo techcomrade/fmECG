aa3e59e7da3575c9ded4a310dca2264fc1d55f41
Duong <duongtoan1k99@gmail.com>
2024-11-22 10:31:40 +0700

    feature(front-end): function for doctors to accept or reject a patient's schedule

--- ui_web/src/components/Modal/ScheduleModal.tsx
+++ ui_web/src/components/Modal/ScheduleModal.tsx
@@ -1,13 +1,55 @@
 import * as React from "react";
-import { Modal, Badge, Card, Tooltip } from "antd";
-import type { BadgeProps } from "antd";
+import { Modal, Card, Tooltip, Button } from "antd";
 import "./schedule.scss";
-import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { CarryOutOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
 import { Context } from "../../utils/context";
 import { userRole } from "../../constants";
+import { useAppDispatch, useAppSelector } from "../../redux/hook";
+import {
+  acceptSchedule,
+  getScheduleByDoctorId,
+  rejectSchedule,
+  resetLoadAcceptScheduleStatus,
+  resetLoadRejectScheduleStatus,
+} from "../../redux/reducer/scheduleSlice";
+import { ApiLoadingStatus } from "../../utils/loadingStatus";
+import { showNotiError, showNotiSuccess } from "../notification";
+import { AcceptScheduleRequest } from "../../api";
 
 const ScheduleModalComponent = (props: any) => {
   const listData = props.getListData(props.selectedDate);
+  const dispatch = useAppDispatch();
+  const scheduleState = useAppSelector((state) => state.schedule);
+
+  React.useEffect(() => {
+    if (scheduleState.loadAcceptScheduleStatus === ApiLoadingStatus.Success) {
+      showNotiSuccess("Bạn đã chấp nhận lịch khám thành công");
+      dispatch(resetLoadAcceptScheduleStatus());
+      dispatch(getScheduleByDoctorId());
+    }
+    if (
+      scheduleState.loadAcceptScheduleStatus === ApiLoadingStatus.Failed &&
+      scheduleState.errorMessage
+    ) {
+      showNotiError(scheduleState.errorMessage);
+      dispatch(resetLoadAcceptScheduleStatus());
+    }
+  }, [scheduleState.loadAcceptScheduleStatus]);
+
+  React.useEffect(() => {
+    if (scheduleState.loadRejectScheduleStatus === ApiLoadingStatus.Success) {
+      showNotiSuccess("Bạn đã hủy lịch khám thành công");
+      dispatch(resetLoadRejectScheduleStatus());
+      dispatch(getScheduleByDoctorId());
+    }
+    if (
+      scheduleState.loadRejectScheduleStatus === ApiLoadingStatus.Failed &&
+      scheduleState.errorMessage
+    ) {
+      showNotiError(scheduleState.errorMessage);
+      dispatch(resetLoadRejectScheduleStatus());
+    }
+  }, [scheduleState.loadRejectScheduleStatus]);
 
   return (
     <Modal
@@ -20,7 +62,7 @@ const ScheduleModalComponent = (props: any) => {
         listData.map(
           (
             item: {
-              type: string;
+              type: any;
               schedule_id: string;
               schedule_type: any;
               patient_id: string;
@@ -69,15 +111,60 @@ const ScheduleModalComponent = (props: any) => {
                       </Tooltip>,
                     ]
                   : []),
+                ...(Context.role === userRole.doctor && item.type === 2
+                  ? [
+                      <Tooltip title="Chấp nhận lịch khám" key="accept">
+                        <CarryOutOutlined
+                          onClick={() => {
+                            Modal.confirm({
+                              title: "Chấp nhận lịch khám",
+                              content:
+                                "Bạn có muốn chấp nhận lịch khám này không?",
+                              footer: (_, { CancelBtn }) => (
+                                <>
+                                  <CancelBtn />
+                                  <Button
+                                    key="not-accept"
+                                    onClick={() => {
+                                      dispatch(
+                                        rejectSchedule(item.schedule_id)
+                                      );
+                                      Modal.destroyAll();
+                                    }}
+                                  >
+                                    Không
+                                  </Button>
+                                  <Button
+                                    key="accept"
+                                    type="primary"
+                                    onClick={() => {
+                                      dispatch(
+                                        acceptSchedule({
+                                          schedule_id: item.schedule_id,
+                                        } as AcceptScheduleRequest)
+                                      );
+                                      Modal.destroyAll();
+                                    }}
+                                  >
+                                    Có
+                                  </Button>
+                                </>
+                              ),
+                            });
+                          }}
+                        ></CarryOutOutlined>
+                      </Tooltip>,
+                    ]
+                  : []),
               ]}
             >
-              <Badge
-                status={item.type as BadgeProps["status"]}
-                text={item.session_string}
-              />
               <div className="event-details">
                 {Object.entries(item).map(([key, value]) => {
-                  if (key == "schedule_type" || key == "status")
+                  if (
+                    key == "session_string" ||
+                    key == "schedule_type" ||
+                    key == "status"
+                  )
                     return <div key={key}>{value}</div>;
                   if (
                     (Context.role === userRole.admin ||
