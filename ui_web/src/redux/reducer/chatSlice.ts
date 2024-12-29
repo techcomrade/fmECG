39e364bc60936241334545a7e49ca8beb0f44ca7
Duong <duongtoan1k99@gmail.com>
2024-12-29 14:33:05 +0700

    improve(front-end): show data rec url with start time and file name

--- ui_web/src/redux/reducer/chatSlice.ts
+++ ui_web/src/redux/reducer/chatSlice.ts
@@ -1,77 +1,76 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { ApiLoadingStatus } from '../../utils/loadingStatus';
-import { createAsyncThunkWrap } from '../handler';
-import { MessageSchema, MessageRequest, Service } from '../../api';
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ApiLoadingStatus } from "../../utils/loadingStatus";
+import { createAsyncThunkWrap } from "../handler";
+import { MessageSchema, MessageRequest, Service } from "../../api";
 
 interface IChatState {
-    messages: MessageSchema[];
-    loadGetMessageStatus: ApiLoadingStatus;
-    loadSendMessageStatus: ApiLoadingStatus;
-    errorMessage: string | undefined;
+  messages: MessageSchema[];
+  loadGetMessageStatus: ApiLoadingStatus;
+  loadSendMessageStatus: ApiLoadingStatus;
+  errorMessage: string | undefined;
 }
 
 const initialState: IChatState = {
-    messages: [],
-    loadGetMessageStatus: ApiLoadingStatus.None,
-    loadSendMessageStatus: ApiLoadingStatus.None,
-    errorMessage: undefined,
+  messages: [],
+  loadGetMessageStatus: ApiLoadingStatus.None,
+  loadSendMessageStatus: ApiLoadingStatus.None,
+  errorMessage: undefined,
 };
 
 export const loadMessages = createAsyncThunkWrap(
-    "/chat/messages",
-    async (messageRequest: { groupChatId: string }) => {
-        return await Service.chatService.loadMessages(messageRequest.groupChatId)
-    }
+  "/chat/messages",
+  async (messageRequest: { groupChatId: string }) => {
+    return await Service.chatService.loadMessages(messageRequest.groupChatId);
+  }
 );
 
 export const sendMessage = createAsyncThunkWrap(
-    "/chat/send",
-    async (messageRequest: MessageRequest) => {
-        return await Service.chatService.sendMessage(messageRequest)
-    }
+  "/chat/send",
+  async (messageRequest: MessageRequest) => {
+    return await Service.chatService.sendMessage(messageRequest);
+  }
 );
 
-
 export const chatSlice = createSlice({
-    name: 'chat',
-    initialState,
-    reducers: {
-        resetGetMessageStatus: (state) => {
-            state.loadGetMessageStatus = ApiLoadingStatus.None;
-        },
-        resetSendMessageStatus: (state) => {
-            state.loadSendMessageStatus = ApiLoadingStatus.None;
-        }
+  name: "chat",
+  initialState,
+  reducers: {
+    resetGetMessageStatus: (state) => {
+      state.loadGetMessageStatus = ApiLoadingStatus.None;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(loadMessages.pending, (state) => {
-                state.loadGetMessageStatus = ApiLoadingStatus.Loading;
-            })
-            .addCase(loadMessages.fulfilled, (state, action) => {
-                state.messages = action.payload;
-                state.loadGetMessageStatus = ApiLoadingStatus.Success;
-            })
-            .addCase(loadMessages.rejected, (state, action) => {
-                state.errorMessage = (action.payload as any)?.message || "Unknown error";
-                state.loadGetMessageStatus = ApiLoadingStatus.Failed;
-            })
-            .addCase(sendMessage.pending, (state) => {
-                state.loadSendMessageStatus = ApiLoadingStatus.Loading;
-            })
-            .addCase(sendMessage.fulfilled, (state, action) => {
-                state.loadSendMessageStatus = ApiLoadingStatus.Success;
-            })
-            .addCase(sendMessage.rejected, (state, action) => {
-                state.errorMessage = (action.payload as any)?.message || "Unknown error";
-                state.loadSendMessageStatus = ApiLoadingStatus.Failed;
-            });
+    resetSendMessageStatus: (state) => {
+      state.loadSendMessageStatus = ApiLoadingStatus.None;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadMessages.pending, (state) => {
+        state.loadGetMessageStatus = ApiLoadingStatus.Loading;
+      })
+      .addCase(loadMessages.fulfilled, (state, action) => {
+        state.messages = action.payload;
+        state.loadGetMessageStatus = ApiLoadingStatus.Success;
+      })
+      .addCase(loadMessages.rejected, (state, action) => {
+        state.errorMessage =
+          (action.payload as any)?.message || "Unknown error";
+        state.loadGetMessageStatus = ApiLoadingStatus.Failed;
+      })
+      .addCase(sendMessage.pending, (state) => {
+        state.loadSendMessageStatus = ApiLoadingStatus.Loading;
+      })
+      .addCase(sendMessage.fulfilled, (state, action) => {
+        state.loadSendMessageStatus = ApiLoadingStatus.Success;
+      })
+      .addCase(sendMessage.rejected, (state, action) => {
+        state.errorMessage =
+          (action.payload as any)?.message || "Unknown error";
+        state.loadSendMessageStatus = ApiLoadingStatus.Failed;
+      });
+  },
 });
 
-export const {
-    resetGetMessageStatus,
-    resetSendMessageStatus
-} = chatSlice.actions;
+export const { resetGetMessageStatus, resetSendMessageStatus } =
+  chatSlice.actions;
 
 export default chatSlice.reducer;
