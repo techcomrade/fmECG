9131c0625074f0ba41e43a6d91a2ea6fae040cc2
thaidmfinnick <thai.dm279@gmail.com>
2025-01-09 00:23:01 +0700

    improve(back-end): update docker flow

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,8 +28,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(port, host, () => {
-    console.log(`Application is running on: http://${host}:${port}`);
+  await app.listen(port, () => {
+    console.log(`Application is runninggg on: http://${host}:${port}`);
   });
 }
 
